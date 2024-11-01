6078c0761a4281eb9611e264ab39df8283433c20
gordonbchen <gordonbchen@gmail.com>
2024-11-01 19:16:03 -0400

    add hw_08 solution

--- hw/hw_08/RecordsMap.py
+++ hw/hw_08/RecordsMap.py
@@ -1,28 +1,105 @@
-# See assignment for class attributes.
-# Remember to include docstrings.
-# Start with unittests
+from __future__ import annotations
+
 
 class LocalRecord:
-    def __init__(self, pos, max=None, min=None, precision = 0): pass
+    """Stores record temperatures for a single grid point."""
 
-    def add_report(self, temp): pass
+    def __init__(
+        self,
+        pos: tuple[float, float],
+        max: float = float("-inf"),
+        min: float = float("inf"),
+        precision: int = 0,
+    ) -> None:
+        """Initialize LocalRecord."""
+        self.pos = (round(pos[0], precision), round(pos[1], precision))
+        self.max = max
+        self.min = min
 
-    def __eq__(self, other): pass
+    def add_report(self, temp: float) -> None:
+        """Update the min and max temp if the temp is a record."""
+        if temp > self.max:
+            self.max = temp
+        if temp < self.min:
+            self.min = temp
 
-    def __hash__(self): pass
+    def __eq__(self, other: LocalRecord) -> bool:
+        """Return True if the records are for the same position."""
+        return self.pos == other.pos
 
-    def __repr__(self):
-        return f"Record(pos={self.pos}, max={self.max}, min={self.min}"
+    def __hash__(self) -> int:
+        """Return the hash of the position coordinate."""
+        return hash(self.pos)
+
+    def __repr__(self) -> str:
+        """Return a str representation of the LocalRecord."""
+        return f"Record(pos={self.pos}, max={self.max}, min={self.min})"
+
 
 class RecordsMap:
-    def __init__(self): pass
+    """A hash map of temperature records."""
 
-    def __len__(self): pass
+    def __init__(self) -> None:
+        """Initialize the temperature record hash map."""
+        self._len = 0
+        self._n_buckets = 4
+        self._buckets: list[list[LocalRecord]] = self._create_empty_buckets()
 
-    def add_report(self, pos, temp): pass
+    def _create_empty_buckets(self) -> list[list[LocalRecord]]:
+        """Create empty buckets storing LocalRecords."""
+        return [list() for i in range(self._n_buckets)]
 
-    def __getitem__(self, pos): pass
-  
-    def __contains__(self, pos): pass
+    def __len__(self) -> int:
+        """Return the number of records in the hash map."""
+        return self._len
 
-    def _rehash(self, m_new): pass
+    def add_report(self, pos: tuple[float, float], temp: float) -> None:
+        """Add a report to the LocalRecord corresponding to the position."""
+        target_lr = LocalRecord(pos)
+        bucket = self._get_bucket(target_lr)
+
+        for lr in bucket:
+            if lr == target_lr:
+                lr.add_report(temp)
+                return
+
+        self._len += 1
+        target_lr.add_report(temp)
+        bucket.append(target_lr)
+
+        if self._len > self._n_buckets:
+            self._rehash(self._n_buckets * 2)
+
+    def _get_bucket(self, lr: LocalRecord) -> list[LocalRecord]:
+        """Return the bucket with the LocalRecord."""
+        bucket_idx = hash(lr) % self._n_buckets
+        return self._buckets[bucket_idx]
+
+    def __getitem__(self, pos: tuple[float, float]) -> tuple[float, float]:
+        """
+        Return the (min, max) temperatures for the position.
+        Raise a KeyError if pos is not in the hashmap.
+        """
+        target_lr = LocalRecord(pos)
+        for lr in self._get_bucket(target_lr):
+            if lr == target_lr:
+                return (lr.min, lr.max)
+        raise KeyError(f"LocalRecord at pos {pos} not found.")
+
+    def __contains__(self, pos: tuple[float, float]) -> bool:
+        """Return True if the hash map contains a LocalRecord for the position."""
+        target_lr = LocalRecord(pos)
+        for lr in self._get_bucket(target_lr):
+            if lr == target_lr:
+                return True
+        return False
+
+    def _rehash(self, new_n_buckets: int) -> None:
+        """Rehash every entry in the hashtable with new_n_buckets."""
+        old_buckets = self._buckets
+        self._n_buckets = new_n_buckets
+        self._buckets = self._create_empty_buckets()
+
+        for bucket in old_buckets:
+            for lr in bucket:
+                self._get_bucket(lr).append(lr)
--- hw/hw_08/TestRecordsMap.py
+++ hw/hw_08/TestRecordsMap.py
@@ -1,25 +1,156 @@
-# Import what you need
-# Include unittests here. Focus on readability, including comments and docstrings.
+import unittest
+
+from RecordsMap import LocalRecord, RecordsMap
+
 
 class TestLocalRecord(unittest.TestCase):
-    def test_init(self):
-        """ADD DOCSTRING"""
+    """Tests for LocalRecord."""
 
-    def test_hash(self):
-        """ADD DOCSTRING"""
+    def test_init(self) -> None:
+        """Test LocalRecord initialization."""
+        lr1 = LocalRecord((10.3, -10.8))
+        self.assertEqual(lr1.pos, (10.0, -11.0))
+        self.assertEqual(lr1.max, float("-inf"))
+        self.assertEqual(lr1.min, float("inf"))
 
-    def test_eq(self):
-        """ADD DOCSTRING"""
+        lr2 = LocalRecord((10.8, -10.2), max=5.0, min=-5.0, precision=1)
+        self.assertEqual(lr2.pos, (10.8, -10.2))
+        self.assertEqual(lr2.max, 5.0)
+        self.assertEqual(lr2.min, -5.0)
 
-    def test_add_report(self):
-        """ADD DOCSTRING"""
+    def test_hash(self) -> None:
+        """Test LocalRecord hashing."""
+        lr1 = LocalRecord((10.3, -10.8))
+        lr2 = LocalRecord((10.4, -10.7), max=5.0, min=-5.0, precision=0)
+        self.assertEqual(hash(lr1), hash(lr2))
+
+        lr3 = LocalRecord((11.0, -11.0))
+        lr4 = LocalRecord((10.0, -10.0))
+        lr5 = LocalRecord((11.0, -10.0))
+        for lr in [lr3, lr4, lr5]:
+            self.assertNotEqual(hash(lr1), hash(lr))
+
+    def test_eq(self) -> None:
+        """Test equality between LocalRecords."""
+        lr1 = LocalRecord((10.3, -10.8))
+        lr2 = LocalRecord((10.4, -10.7), max=5.0, min=-5.0, precision=0)
+        self.assertEqual(lr1, lr2)
+
+        lr3 = LocalRecord((11.0, -11.0))
+        lr4 = LocalRecord((10.0, -10.0))
+        lr5 = LocalRecord((11.0, -10.0))
+        for lr in [lr3, lr4, lr5]:
+            self.assertNotEqual(lr1, lr)
+
+    def test_add_report(self) -> None:
+        """Test adding a report and updating min/max temps."""
+        lr = LocalRecord((10.3, -10.8))
+        self.assertEqual(lr.min, float("inf"))
+        self.assertEqual(lr.max, float("-inf"))
+
+        lr.add_report(100.0)
+        self.assertEqual(lr.min, 100.0)
+        self.assertEqual(lr.max, 100.0)
+
+        lr.add_report(-100.0)
+        self.assertEqual(lr.min, -100.0)
+        self.assertEqual(lr.max, 100.0)
+
+        lr.add_report(200.0)
+        self.assertEqual(lr.min, -100.0)
+        self.assertEqual(lr.max, 200.0)
 
 
 class TestRecordsMap(unittest.TestCase):
+    """Tests for RecordsMap."""
+
+    def test_empty(self) -> None:
+        """Test an empty RecordsMap."""
+        rm = RecordsMap()
+        self.assertEqual(len(rm), 0)
+
+        self.assertFalse((10.0, 10.0) in rm)
+        with self.assertRaises(KeyError):
+            rm[(10.0, 10.0)]
+
     def test_add_one_report(self):
-        """ADD DOCSTRING. Remember to test len, get, contains, and add_report"""
+        """Test adding 1 report."""
+        rm = RecordsMap()
+        rm.add_report((10.3, -10.8), 70.0)
+
+        self.assertEqual(len(rm), 1)
+        self.assertTrue((10.0, -11.0) in rm)
+        self.assertTrue((10.4, -10.7) in rm)
+        self.assertTrue((10.3, -10.8) in rm)
+
+        self.assertFalse((11.0, -10.0) in rm)
+        self.assertFalse((10.6, -10.7) in rm)
+        self.assertFalse((10.3, -10.2) in rm)
+
+        self.assertEqual(rm[(10.0, -11.0)], (70.0, 70.0))
 
     def test_add_many_reports(self):
-        """ADD DOCSTRING. Remember to test len, get, contains, and add_report"""
+        """Test adding multiple reports. Update the same report and add different reports."""
+        rm = RecordsMap()
+        rm.add_report((10.3, -10.8), 70.0)
+        self.assertEqual(len(rm), 1)
+        self.assertEqual(rm[(10.0, -11.0)], (70.0, 70.0))
 
-# You need to add a line here to run the unittests
+        # Add new max.
+        rm.add_report((10.2, -10.7), 90.0)
+        self.assertEqual(len(rm), 1)
+        self.assertEqual(rm[(10.2, -10.7)], (70.0, 90.0))
+
+        # Add new min.
+        rm.add_report((10.0, -11.2), 50.0)
+        self.assertEqual(len(rm), 1)
+        self.assertEqual(rm[(10.0, -11.2)], (50.0, 90.0))
+
+        # Add a different report.
+        rm.add_report((99.3, -99.8), 70.0)
+        self.assertEqual(len(rm), 2)
+        self.assertEqual(rm[(10.0, -11.0)], (50.0, 90.0))
+        self.assertEqual(rm[(99.0, -100.0)], (70.0, 70.0))
+
+        # Test contains.
+        self.assertTrue((10.0, -11.0) in rm)
+        self.assertTrue((99.0, -100.0) in rm)
+        self.assertFalse((50.0, 50.0) in rm)
+
+    def test_rehashing(self):
+        """Test rehashing frequency and functionality."""
+        rm = RecordsMap()
+        rm._rehash = call_counter(rm._rehash)
+        self.assertEqual(rm._rehash.calls, 0)
+
+        rm.add_report((10.3, -10.8), 70.0)
+        self.assertEqual(rm._rehash.calls, 0)
+
+        # Update the report many times. Should not rehash, len same.
+        rm.add_report((10.0, -11.0), 90.0)
+        rm.add_report((9.8, -11.4), 80.0)
+        rm.add_report((10.0, -11.0), 10.0)
+        rm.add_report((10.0, -11.0), 99.0)
+        self.assertEqual(rm._rehash.calls, 0)
+
+        # Add more reports (rehashes after 4, 8, 16, 32, 64, 128).
+        for i in range(240):
+            rm.add_report((i, i), i)
+            self.assertEqual(len(rm), i + 2)
+        self.assertEqual(rm._rehash.calls, 6)
+        self.assertEqual(rm._n_buckets, 128 * 2)
+
+
+def call_counter(func):
+    """A function decorator that counts the number of times the func is called."""
+
+    def helper(*args):
+        helper.calls += 1
+        return func(*args)
+
+    helper.calls = 0
+    return helper
+
+
+if __name__ == "__main__":
+    unittest.main()
