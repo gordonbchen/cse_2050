34e1385e408169164b6727c2b111ba24439d8fd8
gordonbchen <gordonbchen@gmail.com>
2024-11-09 10:14:25 -0500

    add mod_09 notes and implementation

--- notes/mod_09/binary_search_tree.py
+++ notes/mod_09/binary_search_tree.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Iterator
 
 
 class BSTNode:
@@ -16,30 +16,192 @@ class BSTNode:
         self.weight = 1
 
     def get(self, key: Any) -> Any:
-        """Get the value associated with the key."""
-        if self.key == key:
+        """
+        Get the value associated with the key.
+        Raise a KeyError if the key is not found.
+        """
+        if key == self.key:
             return self.value
-        elif (self.key < key) and (self.right is not None):
+        elif (key > self.key) and (self.right is not None):
             return self.right.get(key)
-        elif (self.key > key) and (self.left is not None):
+        elif (key < self.key) and (self.left is not None):
             return self.left.get(key)
 
         raise KeyError(f"Key {key} not found in tree.")
 
-    def put(self, key: Any, value: Any, parent: BSTNode | None = None) -> None:
-        if self.key == key:
+    def put(self, key: Any, value: Any, parent: BSTNode | None = None) -> BSTNode:
+        """
+        Add a key-value pair to the tree.
+        Update the value of a key if the key is already in the BST.
+        """
+        if key == self.key:
             self.value = value
-        elif self.key < key:
-            if self.right:
-                self.right.put(key, value, parent=self)
-            else:
+        elif key > self.key:
+            if self.right is None:
                 self.right = BSTNode(key, value)
-        else:
-            if self.left:
-                self.left.put(key, value, parent=self)
             else:
+                self.right.put(key, value, parent=self)
+        else:
+            if self.left is None:
                 self.left = BSTNode(key, value)
+            else:
+                self.left.put(key, value, parent=self)
+
+        # Rotate to keep tree balanced.
+        left_weight, right_weight = self.update_weights()
+        if ((max(left_weight, right_weight) + 1) / (min(left_weight, right_weight) + 1)) >= 4:
+            if left_weight > right_weight:
+                return self.rotate_right(parent)
+            else:
+                return self.rotate_left(parent)
+
+        return self
+
+    def update_weights(self) -> tuple[int, int]:
+        """Update the node's weight, and return the updated left and right weights."""
+        left_weight = 0 if self.left is None else self.left.weight
+        right_weight = 0 if self.right is None else self.right.weight
+        self.weight = left_weight + right_weight + 1
+        return left_weight, right_weight
+
+    def rotate_left(self, parent: BSTNode | None) -> BSTNode:
+        """Rotate the BST left."""
+        old, new = self, self.right
+        old.right = new.left
+        new.left = old
+
+        old.update_weights()
+        new.update_weights()
+
+        if parent is not None:
+            if self.key < parent.key:
+                parent.left = new
+            else:
+                parent.right = new
+
+        return new
+
+    def rotate_right(self, parent: BSTNode | None) -> BSTNode:
+        """Rotate the BST right."""
+        old, new = self, self.left
+        old.left = new.right
+        new.right = old
+
+        old.update_weights()
+        new.update_weights()
+
+        if parent is not None:
+            if self.key < parent.key:
+                parent.left = new
+            else:
+                parent.right = new
+
+        return new
+
+    def remove(self, key: Any) -> BSTNode | None:
+        """Remove a key from the BST."""
+        if key == self.key:
+            # If there is 1 child, point to that child.
+            # If there are 0 children, point to None.
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.left
+
+            # If there are 2 children, find the max node in the left subtree.
+            # Copy key/value, and remove the copied node.
+            max_node = self.left.get_max_node()
+            self.key = max_node.key
+            self.value = max_node.value
+            self.left = self.left.remove(max_node.key)
+        elif (key < self.key) and (self.left is not None):
+            self.left = self.left.remove(key)
+        elif (key > self.key) and (self.right is not None):
+            self.right = self.right.remove(key)
+        else:
+            raise KeyError(f"Key {key} not found in BST. Could not be removed.")
+
+        return self
+
+    def get_max_node(self) -> BSTNode:
+        """Return the max node in the BST subtree."""
+        if self.right is None:
+            return self
+        return self.right.get_max_node()
+
+    def floor(self, key) -> BSTNode | None:
+        """
+        If the key is in the BST, return the node.
+        Otherwise, return the node with the next lowest key.
+        If there are no keys lower, return None.
+        """
+        if self.key == key:
+            return self
+        elif key < self.key:
+            if self.left is None:
+                return None
+
+            return self.left.floor(key)
+        else:
+            if self.right is None:
+                return self
+
+            right_floor = self.right.floor(key)
+            if right_floor is None:
+                return self
+            return right_floor
+
+    def in_order_traverse(self) -> Iterator[BSTNode]:
+        """Traverse the BST in-order."""
+        if self.left is not None:
+            yield from self.left.in_order_traverse()
+
+        yield self
+
+        if self.right is not None:
+            yield from self.right.in_order_traverse()
+
+    def pre_order_traverse(self) -> Iterator[BSTNode]:
+        """Traverse the BST pre-order."""
+        yield self
+
+        if self.left is not None:
+            yield from self.left.pre_order_traverse()
+
+        if self.right is not None:
+            yield from self.right.pre_order_traverse()
+
+    def __str__(self) -> str:
+        """Return a str representation of the node."""
+        return f"BSTNode({self.key}, {self.value})"
 
 
 if __name__ == "__main__":
-    pass
+    root = BSTNode(8, 8)
+    root = root.put(6, 6)
+    root = root.put(4, 4)
+    root = root.put(5, 5)
+    root = root.put(7, 7)
+
+    for node in root.pre_order_traverse():
+        print(node)
+    print(root.get(5))
+
+    for node in root.pre_order_traverse():
+        print(node)
+
+    root.remove(6)
+    for node in root.pre_order_traverse():
+        print(node)
+
+    for i in [0, 4, 4.5, 100]:
+        print(root.floor(i))
+
+    print("\n\n")
+    root = BSTNode(0, 0)
+    for i in range(10):
+        root = root.put(i, i)
+
+        print(i)
+        for node in root.pre_order_traverse():
+            print(node)
--- notes/mod_09/notes.md
+++ notes/mod_09/notes.md
@@ -32,10 +32,40 @@
     * __iter__: called with iter()
     * __next__: called with next(), produces next item, raises StopIteration when done
     * For is a while loop
-    * Generator: yeild values
+    * Generator: yield values
 * Binary search tree
     * left node is less, right node is greater
     * Tree sort algorithm: in order traverse on BST
     * ADT: put, get, floor, ceil, remove
     * Time complexity based on height of tree: best case log2n, worst case n
-    * Node removal: 
+
+## 11/8/2024
+* Removal
+    * Search for node from root
+    * If leaf node: parent.left = None
+    * If 1 child: replace node with child
+    * If 2 children: swap with largest key in left subtree (left, then right recursively), then call remove again on the lelft subtree.
+        * Or the smallest key in the right subtree (right once, then left recursively)
+* Floor: return None or greatest floor value
+* In-order traverse: will always get elements in order (left, root, right)
+* Time complexity always based on tree height (log2n best, n worst)
+* Balancing: Left/right rotation, constant time
+
+# 11/9/2024 (BST review)
+* Left subtree only contains keys less than, right subtree only contains keys greater than
+* In-order traverse: left, root, right
+* get, put: O(length of longest path) = O(height of tree)
+* remove:
+    * leaf: simple deletion: parent.left = None
+    * 1 child: move child up: parent.left = self.left
+    * find greatest value in left subtree (or lowest value in right subtree), swap with node to be removed, and then call remove on the left subtree (will be leaf node)
+    * O(tree height)
+* floor: return node with matching key or next lowest (max less than)
+    * return None if none are lower
+* Worst case: O(tree height) = O(n), when keys are inserted in order.
+* Rotations: weight balancing O(1)
+    * Right rotation: self.right = self.parent, self.parent.left = self.old_right
+    * Explanation: the parent will be greater than the node (node is in the left subtree when right rotating), so parent will be self.right. The old right was in the left subtree of the parent, so now make it the left node of the parent. 
+    * Left rotation: self.left = self.parent, self.parent.right = self.old_left
+* Height-balanced tree: AVL trees, self-balancing BST
+    * O(logn) get, put, remove
